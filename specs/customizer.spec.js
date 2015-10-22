15f730ffe75b87c6ad3505a1f164015ee87dc666
Max Gordon <gordonmaxc@gmail.com>
2015-10-22 15:32:50 -0700

    1 Working test
    Signed-off-by: Heather Farley <hrfarley@gmail.com>

--- specs/customizer.spec.js
+++ specs/customizer.spec.js
@@ -1,7 +1,18 @@
+import React from 'react'
+import ReactTestUtils from 'react-addons-test-utils'
 import Customizer from '../public/scripts/customizer.js'
 
+let customizer;
+
 describe('Customizer', () => {
-  it('should be working', () => {
-    expect(true).toBe(false);
+
+  beforeEach(()=>{
+    const shallowRenderer = ReactTestUtils.createRenderer();
+    shallowRenderer.render(React.createElement(Customizer, { className: 'MyComponent' }, 'some child text'));
+    customizer = shallowRenderer.getRenderOutput();
+  });
+
+  it('should display text', () => {
+    expect(customizer.props.children).toEqual('Hello World');
   })
 });
